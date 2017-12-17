c335b69fba0a7f925a24ed67bc659f462ddba4f5
def327 <def3273@gmail.com>
2017-12-17 20:35:07 +0300

    init Online Library commit v1.0

---
+++ src/main/java/com/def327/project/library/OnlineLibraryApplication.java
@@ -0,0 +1,12 @@
+package com.def327.project.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineLibraryApplication.class, args);
+	}
+}
---
+++ src/main/java/com/def327/project/library/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.def327.project.library;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(OnlineLibraryApplication.class);
+	}
+
+}
